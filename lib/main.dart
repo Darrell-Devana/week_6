33a366e46a8c58f2039bf0c34ae6f32f277ce52b
Darrell-Devana <darrell.devanaputra@binus.ac.id>
2023-03-21 20:02:42 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,17 +34,68 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  void onSubmitted(String value) {
+    setState(() {
+      NameFile.name = value;
+    });
+  }
+
+  var _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Input App'),
+        title: const Text('Input App'),
       ),
-      body: Container(
-        child: Center(
-          child: const Text('Hello World'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: _controller.clear,
+                      icon: const Icon(Icons.clear),
+                    ),
+                    border: const OutlineInputBorder(),
+                    labelText: 'Input Name',
+                  ),
+                  onSubmitted: onSubmitted),
+            ),
+            Card(
+              color: Colors.blue,
+              child: Container(
+                width: 200,
+                height: 60,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text('Hello ${NameFile.name}',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  onSubmitted(_controller.text);
+                });
+              },
+              child: Text('Submit'),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class NameFile {
+  static String name = "";
 }
