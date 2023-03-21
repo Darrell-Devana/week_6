cb624989e916018edfe79deaecf5146deb4fe494
Darrell-Devana <darrell.devanaputra@binus.ac.id>
2023-03-21 14:06:40 +0700

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# week_6
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        canvasColor: Colors.white,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input App'),
+      ),
+      body: Container(
+        child: Center(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
